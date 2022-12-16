2d3789d19ac82f0098cfb7213b5b7bd9ec5cd7d5
YasharHND <yashar@sandboxapp.io>
2022-12-16 11:51:00 +0300

    2

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
 n1 = 1
 n2 = 2
-sum_of_even = 0
+out = 0
 while n2 < 4000000:
     if n2 % 2 == 0:
-        sum_of_even += n2
+        out += n2
     n1, n2 = n2, n1 + n2
-print(sum_of_even)
+print(out)
