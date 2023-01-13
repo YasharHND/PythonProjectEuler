5dac05d41d87fce92564998bc6261b73556789ab
YasharHND <yashar@sandboxapp.io>
2023-01-13 11:45:42 +0300

    4

--- 4.py
+++ 4.py
@@ -1,15 +1,17 @@
-def is_palindromic(number):
-    str_num = str(number)
-    for i in range(len(str_num) // 2):
-        if str_num[i] != str_num[-1 - i]:
+def is_palindrome(number):
+    str_number = str(number)
+    for i in range(len(str_number) // 2):
+        if str_number[i] != str_number[-1 - i]:
             return False
     return True
 
 
-palindromic_numbers = []
-for x in reversed(range(100, 1000)):
-    for y in reversed(range(100, 1000)):
+largest_palindrome = 0
+for x in range(1, 1000):
+    for y in range(1, 1000):
         product = x * y
-        if is_palindromic(product):
-            palindromic_numbers.append(product)
-print(max(palindromic_numbers))
+        if is_palindrome(product):
+            if product > largest_palindrome:
+                largest_palindrome = product
+
+print(largest_palindrome)
