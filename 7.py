d2fcebce5f8259e4fa53606d0d27a7fcad6616f8
YasharHND <yashar@sandboxapp.io>
2023-01-23 10:53:31 +0300

    7

--- 7.py
+++ 7.py
@@ -4,14 +4,14 @@ import math
 def is_prime(number):
     if number % 2 == 0:
         return False
-    for i in range(2, int(math.sqrt(number) + 1)):
+    for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
     return True
 
 
-prime_count = 1
 n = 2
+prime_count = 1
 while prime_count < 10001:
     n += 1
     if is_prime(n):
