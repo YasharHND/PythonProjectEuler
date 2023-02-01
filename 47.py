399f8411bf14a0c0edaafd58a62e5faf7bfec493
YasharHND <yashar@sandboxapp.io>
2023-02-01 16:52:18 +0300

    47

---
+++ 47.py
@@ -0,0 +1,28 @@
+import math
+
+
+def distinct_prime_factors(n):
+    out = set()
+    while n % 2 == 0:
+        out.add(2)
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            out.add(i)
+            n = n / i
+    if n > 2:
+        out.add(int(n))
+    return out
+
+
+target = []
+k = 1
+while True:
+    if len(distinct_prime_factors(k)) == 4:
+        target.append(k)
+        if len(target) == 4:
+            print(target[0])
+            break
+    else:
+        target.clear()
+    k += 1
