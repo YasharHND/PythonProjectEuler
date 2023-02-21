475168a5c8941639a1c6199003e8cec083ef76f7
YasharHND <yashar@sandboxapp.io>
2023-02-21 15:16:24 +0300

    project euler problem 69

--- 47.py
+++ 47.py
@@ -1,20 +1,4 @@
-import math
-
-
-def distinct_prime_factors(n):
-    out = set()
-    if n % 2 == 0:
-        out.add(2)
-    while n % 2 == 0:
-        n = n / 2
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            out.add(i)
-            n = n / i
-    if n > 2:
-        out.add(int(n))
-    return out
-
+from primes import distinct_prime_factors
 
 target = []
 k = 1
---
+++ 69.py
@@ -0,0 +1,30 @@
+import math
+import time
+
+from primes import distinct_prime_factors
+
+
+# https://en.wikipedia.org/wiki/Euler%27s_totient_function
+def totient(number):
+    product = number
+    for factor in distinct_prime_factors(number):
+        product *= 1 - (1 / factor)
+    return math.ceil(product)
+
+
+def solve():
+    target_n = 0
+    maximum = 0
+    for n in range(2, 1_000_001):
+        n_tn = n / totient(n)
+        if n_tn > maximum:
+            maximum = n_tn
+            target_n = n
+    return target_n
+
+
+srt = time.time()
+answer = solve()
+end = time.time()
+print(answer)
+print(f"Took {end - srt} seconds to execute...")
